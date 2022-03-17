f9b5f18a29c7e2e6467531ed457bc35b15ae0a37
Pedro Sandes <pedroalves@pecege.com>
2022-03-17 20:22:50 -0300

    feat: Criar componente SectionTitle

---
+++ src/components/SectionTitle/index.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "SectionTitle"
+}
+</script>
+
+<template>
+  <div class="c-sectiont-title">
+    <h2>
+      <slot></slot>
+    </h2>
+  </div>
+</template>
+
+<style src="./styles.scss" lang="scss" scoped />
+  
