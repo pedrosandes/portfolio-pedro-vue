7e4a4678868a42b16ef477bfa741e865d0131e1a
Pedro Sandes <pedroalves@pecege.com>
2022-03-17 20:22:00 -0300

    feat: Criar componente Footer

---
+++ src/components/TheFooter/index.vue
@@ -0,0 +1,39 @@
+<script>
+import mediaSociais from "@/assets/mock/mediasSociais.js"
+export default {
+  name: "TheFooter",
+  data(){
+    return {
+      medias: mediaSociais,
+    }
+  },
+  computed: {
+    currentYear(){
+      return new Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<template>
+  <footer>
+    <div class="author">
+      <h2>Pedro Sandes</h2>
+      <p>Front-end Developer</p>
+    </div>
+      <nav aria-label="Links sociais">
+        <ul class="social">
+          <li v-for="media in medias" :key="media.id" >
+            <a :href="media.link" target="_blank" rel="noreferrer">
+              <img :src="media.icon" :alt="media.alt">
+            </a>
+          </li>
+        </ul>
+      </nav>
+    <div>
+      <p>Todos os direitos reservados ©  Pedro Sandes {{ currentYear }}</p>
+    </div>
+  </footer>
+</template>
+
+<style src="./styles.scss" lang="scss" scoped/>
