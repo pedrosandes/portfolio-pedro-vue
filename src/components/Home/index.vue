66011c9a197f44475c95f6f82af49ce0dec86eb4
Pedro Sandes <pedroalves@pecege.com>
2022-03-17 20:23:34 -0300

    feat: Criar section de Home

---
+++ src/components/Home/index.vue
@@ -0,0 +1,27 @@
+<script>
+import Button from "@/components/Button"
+
+export default {
+  name: "Main", 
+  components: {
+    Button
+  },
+  methods: {
+    goAboutMe(){
+      location.href = "/#about"
+    }
+  } 
+}
+</script>
+
+<template>
+  <section id="home" class="c-main">
+    <img title="Imagem de avatar" class="avatar" src="@/assets/images/avatar.png"  />
+    <div class="main-text-content">
+      <h1 class="title">Olá me chamo <span class="decoration-text">Pedro</span>, e sou <span class="decoration-text">Front-end Developer</span>.</h1>
+      <Button v-on:click="goAboutMe">Sobre mim</Button>
+    </div>
+  </section>
+</template>
+
+<style src="./styles.scss" lang="scss" scoped/>
