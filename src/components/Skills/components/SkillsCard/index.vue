7e812eaf8b9b7892074ce9431c00eb5ef252593f
Pedro Sandes <pedroalves@pecege.com>
2022-03-17 20:22:20 -0300

    feat: Criar componente SkillCard

---
+++ src/components/Skills/components/SkillsCard/index.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "SkillsCard",
+  props: {
+    title: String,
+    description: String,
+    image: String,
+    alt: String,
+  }
+}
+</script>
+
+<template>
+  <article class="c-card">
+    <figure>
+      <img :src="image" :alt="alt">
+    </figure>
+    <div class="c-texts">
+      <h3 aria-label="Titulo de uma tecnologia" >{{ title }}</h3>
+      <p>{{ description }}</p>
+    </div>
+  </article>
+</template>
+
+<style src="./styles.scss" lang="scss" scoped />
