f7927abb45b8b464b8eea12c80f93c88f4160d1d
Pedro Sandes <pedroalves@pecege.com>
2022-03-17 20:23:22 -0300

    feat: Criar card de projeto de Portfólio

---
+++ src/components/Portfolio/components/PortfolioProject/index.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "PortfolioProject",
+  props: {
+    projectName: String,
+    projectImage: String,
+    projectAlt: String,
+    projectLink: String
+  },
+  methods: {
+    openLink(){
+      open(this.projectLink, '_blank')
+    }
+  }
+}
+</script>
+
+<template>
+  <article class="c-project">
+    <h3 class="c-project__title">{{ projectName }}</h3>
+    <figure>
+      <img :src="projectImage" :alt="projectAlt">
+    </figure>
+    <button  @click="openLink" :aria-label="`Ver projeto ${projectName}`" class="button">Ver projeto</button>
+  </article>
+</template>
+
+<style src="./styles.scss" lang="scss" scoped/>
