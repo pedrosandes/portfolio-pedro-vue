af4dc75061321238cb270803788498c76cfedf67
Pedro Sandes <pedroalves@pecege.com>
2022-03-17 20:23:43 -0300

    feat: Criar componente Button

---
+++ src/components/Button/index.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "Button",
+  props: {
+   styleClass: String
+  }
+}
+</script>
+
+<template>
+  <button type="button" :class="styleClass">
+    <slot></slot>
+  </button>
+</template>
+
+<style src="./styles.scss" lang="scss" scoped />
+ 
