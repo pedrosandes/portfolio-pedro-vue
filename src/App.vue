cedc4ed0ede92d92aa4e356816fb8488ffc6d8a9
Pedro Sandes <pedroalves@pecege.com>
2022-03-17 20:25:56 -0300

    feat: Implementar componentes de section

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "@/components/TheHeader"
+import Home from "@/components/Home"
+import About from "@/components/About"
+import Portfolio from "@/components/Portfolio"
+import Skills from "@/components/Skills"
+import TheFooter from "@/components/TheFooter"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader,
+    Home,
+    About,
+    Portfolio,
+    Skills,
+    TheFooter
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+    <TheHeader />
+    <main>
+      <Home />
+      <About />
+      <Portfolio />
+      <Skills />
+    </main>
+    <TheFooter />
+</template>
+
+<style src="@/assets/scss/_reset.scss" lang="scss" />
+
+ 
+
+
